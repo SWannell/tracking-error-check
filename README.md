8eec8c053703ca767eb6d80fc8f1e4fc7d023ecc
Sami Wannell <SWannell@redcross.org.uk>
2020-04-20 11:30:30 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# tracking-error-check
-Check tracking errors in Google Analytics
+# Check tracking errors in Google Analytics
+1. Put the minified Javascript into a bookmark, starting `javascript:`
+2. Click the bookmark to load a Query Explorer page with the filters applied.
---
+++ checkMissingAud.js
@@ -0,0 +1,10 @@
+function checkMissingAudience() {
+	var d1 = new Date();
+	endDate = d1.toISOString().substring(0,10);
+	var d2 = new Date(d1.getFullYear(), d1.getMonth(), d1.getDate()-7);
+	startDate = d2.toISOString().substring(0,10);
+	var url2 = "https://ga-dev-tools.appspot.com/query-explorer/?ids=ga%3A149197394&start-date=2020-01-06&end-date=2020-01-12&metrics=ga%3Asessions&dimensions=ga%3Acampaign%2Cga%3AsourceMedium%2Cga%3Akeyword%2Cga%3AadContent&filters=ga%3Akeyword%3D~%5E%5Cd%7B6%7D_%24&segment=gaid%3A%3A-1&samplingLevel=HIGHER_PRECISION&max-results=100000";
+	var newurl = url2.substring(0, 79) + startDate + url2.substring(89, 99) + endDate + url2.substring(109, );
+	return newurl;
+	};
+window.open(checkMissingAudience());
